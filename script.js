582bea33d006c82a1967eb03f8ccfdfc870e363c
Samantha Davidson <samanthajanedavidson@gmail.com>
2022-09-27 17:19:13 -0400

    Updated start button and timer

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 console.log("Anthony's questions");
 
+//Start quiz
+var startBtn = document.getElementById("start-btn");
+startBtn.addEventListener("click", setTime);
+
+
+//Countdown timer
+var timer = document.getElementById("timer");
+
+var secondsLeft = 10;
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timer.textContent = secondsLeft;
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      gameOver();
+    }
+
+  }, 1000);
+
+function gameOver() {
+  timer.textContent = "Time is up!";
+  //present score somehow 
+}
+}
+
+//Question elements 
+var questionListEl = document.createElement("ol");
+
+//Question list items 
+var question1 = document.createElement("li");
+var question2 = document.createElement("li");
+var question3 = document.createElement("li");
+var question3 = document.createElement("li");
+
 //Quiz questions 
 var quizQuestions = [
   {
@@ -29,31 +66,3 @@ var startBtn = document.querySelector("#start-btn");
 var quizContent = document.querySelector("#quiz-questions");
 var choices =  document.querySelector("choices");
 
-//Start quiz 
-function startQuiz(){
-  startBtn.addEventListener("click", setTime());
-}
-
-//Countdown timer
-var timer = document.getElementById("timer");
-
-var secondsLeft = 10;
-
-function setTime() {
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timer.textContent = secondsLeft;
-
-    if(secondsLeft === 0) {
-      clearInterval(timerInterval);
-      gameOver();
-    }
-
-  }, 1000);
-}
-
-function gameOver() {
-  timer.textContent = "Time is up!";
-}
-
-setTime();
