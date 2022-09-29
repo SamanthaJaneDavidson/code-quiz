f0109d820a0ff7047dea738429685152510c9286
Samantha Davidson <samanthajanedavidson@gmail.com>
2022-09-28 22:19:41 -0400

    Moved timer function call, updated quiz questions, added quiz content text

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 console.log("Anthony's questions");
+//Access HTML elements
+var startBtn = document.querySelector("#start-btn");
+var quizContent = document.querySelector("#quiz-content");
+var quizQuestionContainer = document.querySelector("#quiz-questions");
+var choices =  document.querySelector("#choices");
+var currentQuestion = 0;
+
+// var startBtn = document.getElementById("start-btn");
+// var quizContent = document.getElementById("quiz-content");
+// var quizContent = document.getElementById("quiz-questions");
+// var choices =  document.getElementById("choices");
+
 
 //Start quiz
 var startBtn = document.getElementById("start-btn");
-startBtn.addEventListener("click", setTime);
+startBtn.addEventListener("click", startQuiz);
 
+function startQuiz(){
+  setTime();
+  startBtn.setAttribute("style", "display: none;");
+  getQuestions()
+
+ }
 
 //Countdown timer
 var timer = document.getElementById("timer");
@@ -21,48 +39,76 @@ function setTime() {
     }
 
   }, 1000);
-
-function gameOver() {
-  timer.textContent = "Time is up!";
-  //present score somehow 
-}
 }
 
-//Question elements 
+function getQuestions() {
+  // question1: "Placeholder 1",
+  // choices: [
+  //   {text: "a", correct: false},
+  //   {text: "b", correct: false},
+  //   {text: "c", correct: true},
+  //   {text: "d", correct: false},
+  // ]
+  var question = quizQuestions[currentQuestion];
+  quizQuestionContainer.textContent = question.question;
+
+}
+
+//Question elements
 var questionListEl = document.createElement("ol");
 
-//Question list items 
+//Question list items
 var question1 = document.createElement("li");
 var question2 = document.createElement("li");
 var question3 = document.createElement("li");
 var question3 = document.createElement("li");
 
-//Quiz questions 
+//Quiz questions
 var quizQuestions = [
   {
-      question1: "Placeholder 1",
-      choices: ["a", "b", "c", "d"],
-      answer: "a"
+      question: "Placeholder 1",
+      choices: [
+        {text: "a", correct: false},
+        {text: "b", correct: false},
+        {text: "c", correct: true},
+        {text: "d", correct: false},
+      ]
   }, {
-      question2: "Placeholder 2",
-      choices: ["a", "b", "c", "d"],
-      answer: "a"
+      question: "Placeholder 2",
+      choices: [
+        {text: "a", correct: true},
+        {text: "b", correct: false},
+        {text: "c", correct: true},
+        {text: "d", correct: false},
+      ]
   }, {
-      question3: "Placeholder 3",
-      choices: ["a", "b", "c", "d"],
-      answer: "a"
+      question: "Placeholder 3",
+      choices: [
+        {text: "a", correct: true},
+        {text: "b", correct: false},
+        {text: "c", correct: true},
+        {text: "d", correct: false},
+      ]
   }, {
-      question4: "Placeholder 4",
-      choices: ["a", "b", "c", "d"],
-      answer: "a"
+      question: "Placeholder 4",
+      choices: [
+        {text: "a", correct: true},
+        {text: "b", correct: false},
+        {text: "c", correct: true},
+        {text: "d", correct: false},
+      ]
   }, {
-      question5: "Placeholder 5",
-      choices: ["a", "b", "c", "d"],
-      answer: "a"
+      question: "Placeholder 5",
+      choices: [
+        {text: "a", correct: true},
+        {text: "b", correct: false},
+        {text: "c", correct: true},
+        {text: "d", correct: false},
+      ]
   }]
 
-//Access HTML elements
-var startBtn = document.querySelector("#start-btn");
-var quizContent = document.querySelector("#quiz-questions");
-var choices =  document.querySelector("choices");
-
+  //Game over timer
+  function gameOver() {
+    timer.textContent = "Time is up!";
+    //present score somehow
+  }
